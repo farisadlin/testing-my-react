6076c1aa65d5da77ab7a5b7e12f093092c7e52dd
shifair <farisnura@gmail.com>
2021-08-03 23:39:38 +0700

    Added a new test for snapshot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { MyForm } from "./components/MyForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <MyForm />
+    </>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,45 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+// myForm.test.js
+import React from "react";
+import { render, screen, waitFor, cleanup } from "@testing-library/react";
+import renderer from 'react-test-renderer'
+import userEvent from "@testing-library/user-event";
 
-test("renders App component", () => {
-  render(<App />);
+import { MyForm } from "./components/myForm.js";
 
-  
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+let renderLogin, handleSubmit;
+
+beforeEach(() => {
+  handleSubmit = jest.fn();
+  renderLogin = render(<MyForm withSubmit={handleSubmit} />);
 });
+
+afterEach(cleanup);
+
+describe("render logic correctly", () => {
+  it("rendering and submitting a basic Formik form", async () => {
+    userEvent.type(renderLogin.getByLabelText(/first name/i), "John");
+    userEvent.type(renderLogin.getByLabelText(/last name/i), "Dee");
+    userEvent.type(
+      renderLogin.getByLabelText(/email/i),
+      "john.dee@someemail.com"
+    );
+
+    userEvent.click(renderLogin.getByRole("button", { text: /submit dong/i }));
+
+    await waitFor(() => {
+      expect(handleSubmit).toHaveBeenCalledTimes(1);
+      expect(handleSubmit).toHaveBeenCalledWith({
+        email: "john.dee@someemail.com",
+        firstName: "John",
+        lastName: "Dee",
+      });
+    });
+  });
+});
+
+describe("renders UI correctly", () => {
+  it("render whole UI with snapshot", () => {
+    const tree = renderer.create(<MyForm  />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
